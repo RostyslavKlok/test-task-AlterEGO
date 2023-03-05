904788f2ee53439a78e91ecd5aa8983cf0b67677
RostyslavKlok <rostykklok2699@gmail.com>
2023-03-05 18:16:03 +0200

    Added hash router

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import App from "./App";
 import store from "./redux/store/store";
 import reportWebVitals from "./reportWebVitals";
@@ -13,9 +13,9 @@ const root = ReactDOM.createRoot(
 );
 root.render(
   <Provider store={store}>
-    <BrowserRouter>
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </Provider>
 );
 
